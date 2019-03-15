5589b4aa661b5e40833ba992aff9422be21885d3
Harmeet Benawra <harmeetbenawra@gmail.com>
2019-03-15 21:59:33 +0300

    wip: - added admin form to add books to customer checkout

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Book, Customer, CustomerCheckoutHistory, BookCheckoutHistory
+
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Customer)
+
+
+class BookCheckoutHistoryInline(admin.TabularInline):
+    model = BookCheckoutHistory
+    fields = ['book']
+
+
+@admin.register(CustomerCheckoutHistory)
+class CustomerCheckoutHistoryAdmin(admin.ModelAdmin):
+    inlines = [BookCheckoutHistoryInline]
