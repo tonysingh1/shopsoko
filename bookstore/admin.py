97b61d0a63a7acaf5733de75562714d3c7ef6aa6
Harmeet Benawra <harmeetbenawra@gmail.com>
2019-03-16 23:29:49 +0300

    wip: - added method to calculate book charge when returned - able to display fields dynamically - able to create date returned cost when books are returned

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Book, Customer, CustomerCheckoutHistory, BookCheckoutHistory
+from django.db.models.aggregates import Sum
+
+from .models import Book, Customer, CustomerCheckoutHistory, BookCheckoutHistory, DateReturnedCost
 
 
 # Register your models here.
@@ -9,9 +11,48 @@ admin.site.register(Customer)
 
 class BookCheckoutHistoryInline(admin.TabularInline):
     model = BookCheckoutHistory
-    fields = ['book']
+
+    def get_fields(self, request, obj=None):
+        if obj:
+            fields = ['book', 'date_returned']
+        else:
+            fields = ['book']
+        return fields
 
 
 @admin.register(CustomerCheckoutHistory)
 class CustomerCheckoutHistoryAdmin(admin.ModelAdmin):
     inlines = [BookCheckoutHistoryInline]
+    fields = ['customer', 'check_out_date', 'latest_charge']
+    readonly_fields = ['latest_charge']
+
+    def latest_charge(self, obj=None):
+        last = obj.datereturnedcost_set.last()
+        total = BookCheckoutHistory.objects.filter(date_return_cost=last).aggregate(amount=Sum('book_charge'))
+        total = total.get('amount', 0)
+        return total
+
+    def save_formset(self, request, form, formset, change):
+        check_out_history = form.instance
+        instances = formset.save(commit=False)  # gets instance from memory and add to it before saving it
+        for obj in formset.deleted_objects:
+            obj.delete()
+
+        create_date_returned = False
+        date_returned_object = None
+        for instance in instances:
+            if instance.date_returned:
+                if not create_date_returned:
+                    # create date returned cost object
+                    date_returned_object = DateReturnedCost.objects.create(check_out_history=check_out_history)
+                    instance.date_return_cost = date_returned_object
+                else:
+                    instance.date_return_cost.id = date_returned_object
+            instance.save()
+        formset.save_m2m()
+
+
+
+
+
+
